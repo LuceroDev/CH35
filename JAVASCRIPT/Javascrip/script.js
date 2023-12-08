d21fbb6690547c7f1880b4d7ce83fc29a1ac3d9c
Lucr0 <lucero.rosajel@gmail.com>
2023-12-08 16:14:49 -0600

    Tipos de datos y variables con JS

--- JAVASCRIPT/Javascrip/script.js
+++ JAVASCRIPT/Javascrip/script.js
@@ -23,16 +23,70 @@ multiliena se usa  como este comentario*/
 //console.log("multiplicación:" + (numero1 * numero2));
 //console.log("division:" + (numero1 / numero2));
 
-
+//Ejercicio:
 // Solicitar la edad al usuario
-var nombre = prompt("Ingresa tu nombre");
+/*
+Este programa valida si una persona 
+es baby boomer, generacion X, millenial o centenial
+*/
 
-var edad = parseInt(prompt("Ingresa tu edad"));
+// Declaracion de variables
+var nombre, edad, generacion;
 
-if ( edad < 17 ){
+// Función para determinar la generación
+function determinarGeneracion(nombre, fecha_nacimiento) {
+    // Validar que la edad sea un número positivo
+    if (isNaN(fecha_nacimiento) || edad < 0) {
+      return "Por favor, introduce una edad válida.";
+    }
+  
+    // Determinar la generación
+    if (fecha_nacimiento >= 1946 && fecha_nacimiento <= 1964) {
+      generacion = "Baby Boomer";
+    } else if (fecha_nacimiento >= 1965 && fecha_nacimiento <= 1980) {
+      generacion = "Generación X";
+    } else if (fecha_nacimiento >= 1981 && fecha_nacimiento <= 1996) {
+      generacion = "Millennial";
+    } else if (fecha_nacimiento >= 1997) {
+      generacion = "Generación Z (Centennial)";
+    } else {
+      generacion = "No se puede determinar la generación para esta edad.";
+    }
+  
+    // Devolver el resultado
+    return Hola, ${nombre}, tu perteneces a la generación ${generacion}.;
+  }
+  
 
-    alert(nombre + ". Tas chavo, chavo");
-}
-else{
-    alert(nombre + ". Ya te sabes la de chambear");
-}
+  var nombre = prompt("Introduce tu nombre: "); 
+  var fecha_nacimiento = parseInt(prompt("Introduce tu fecha de nacimieno: "));
+  var resultado = determinarGeneracion(nombre, fecha_nacimiento);
+//   Imprime el resultado en la consola
+  console.log(resultado);
+
+
+//TIPO DE DATOS Y VARIABLES
+//scope es el alcance de nustras variables
+
+/*Var, let, const la diferencia el el alcance
+var tiene un alcance global
+let tiene un alcance local
+const se mantiene sin ningun cambio*/
+
+/*string: cadena de texto (lleva comillas)
+number: número
+boolena: true/false
+null:nulo
+undefinied:no esta asigando o definido*/
+
+
+/*Documentación: https://www.freecodecamp.org/espanol/news/var-let-y-const-cual-es-la-diferencia/*/
+
+//Ejemplo: Hacer una fista para Daniel
+
+let nombre = "Daniel"; 
+let invitadxsExtras = 2;
+let esMayorEdad = true;
+let edad = 25;
+let invitadxEspecial = null; //No lo sabemos pero tenenmos la cajita por si acaso
+let horaDeSalida = undefined; //No sabemos pero al final posiblemente tendranun valor
