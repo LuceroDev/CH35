70bea48d41e2ec1d9ce874d12335878ee0504fa0
Lucr0 <lucero.rosajel@gmail.com>
2023-12-08 02:26:05 -0600

    Portafolio de evidencias/tareas

---
+++ JAVASCRIPT/Javascrip/script.js
@@ -0,0 +1,38 @@
+//Comentarios de una sola liena de usan dos diagonales
+/* para comentarios
+multiliena se usa  como este comentario*/
+
+// la siguiente liena es para ver si enlace bien con html
+
+//alert("Hola mundo");
+
+//La siguiente liena e spara mandar un mensaje en mi consola, me puede decir mis errores y en que línea es 
+//console.log("Este es un mensaje para mi consola")
+
+//declaramos variable y usaremos promp que va a mandar mensaje en la mantalla
+//var nombre = prompt("Ingresa tu nombre")
+//alert("Hola, " + nombre + " Bienvenido a la ch35");
+
+
+//var numero1 = parseInt(prompt("Ingrese el primer número"));
+//var numero2 = parseInt(prompt("Ingrese el segundo número"));
+
+
+//console.log ("suma:" + (numero1 + numero2));
+//console.log("resta:" + (numero1 - numero2));
+//console.log("multiplicación:" + (numero1 * numero2));
+//console.log("division:" + (numero1 / numero2));
+
+
+// Solicitar la edad al usuario
+var nombre = prompt("Ingresa tu nombre");
+
+var edad = parseInt(prompt("Ingresa tu edad"));
+
+if ( edad < 17 ){
+
+    alert(nombre + ". Tas chavo, chavo");
+}
+else{
+    alert(nombre + ". Ya te sabes la de chambear");
+}
